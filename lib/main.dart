985a6b082d58f93d8cec68dd3e8d179b6f5a8196
mt347429-hub <mt347429@gmail.com>
2026-02-09 00:25:40 +0200

    init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('note_box');
   runApp(const MyApp());
 }
 
