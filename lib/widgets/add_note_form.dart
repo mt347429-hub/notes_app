3d135b46ee6c2c3ec8bd4cd48ccf86ac58db2057
mt347429-hub <mt347429@gmail.com>
2026-02-09 19:55:22 +0200

    refactor

---
+++ lib/widgets/add_note_form.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'custom_button.dart';
+import 'custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> key = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subsTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: key,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          CustomTextField(
+            text: 'Title',
+            onSaved: (val) {
+              title = val;
+            },
+          ),
+          SizedBox(height: 16),
+          CustomTextField(
+            text: 'Content',
+            maxLine: 5,
+            onSaved: (val) {
+              subsTitle = val;
+            },
+          ),
+          SizedBox(height: 32),
+          CustomButton(
+            onTap: () {
+              if (key.currentState!.validate()) {
+                key.currentState!.save();
+              } else {
+                setState(() {
+                  autovalidateMode = AutovalidateMode.always;
+                });
+              }
+            },
+          ),
+          SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+}
