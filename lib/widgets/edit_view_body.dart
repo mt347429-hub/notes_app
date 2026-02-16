6ac3f2b4d9488b45f6f64bd8215e631d4c94872a
mt347429-hub <mt347429@gmail.com>
2026-02-16 18:31:57 +0200

    change the onchange to controller

--- lib/widgets/edit_view_body.dart
+++ lib/widgets/edit_view_body.dart
@@ -13,42 +13,48 @@ class EditViewBody extends StatefulWidget {
   State<EditViewBody> createState() => _EditViewBodyState();
 }
 
-String? title;
-String? subTitle;
 
 class _EditViewBodyState extends State<EditViewBody> {
+  late TextEditingController titleController;
+  late TextEditingController subTitleController;
+  @override
+  void initState() {
+    super.initState();
+    titleController = TextEditingController(text: widget.note.title);
+    subTitleController=TextEditingController(text: widget.note.subtitle);
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: Column(
-        children: [
-          SizedBox(height: 50),
-          CustomContainer(
-            icon: Icons.check,
-            onPressed: () {
-              widget.note.title = title ?? widget.note.title;
-              widget.note.subtitle = subTitle ?? widget.note.subtitle;
-              Navigator.pop(context);
-              BlocProvider.of<GetNoteCubit>(context).getAllNote();
-            },
-          ),
-          SizedBox(height: 12),
-          CustomTextField(
-            text: widget.note.title,
-            onChanged: (val) {
-              title = val;
-            },
-          ),
-          SizedBox(height: 16),
-          CustomTextField(
-            text: widget.note.subtitle,
-            maxLine: 5,
-            onChanged: (val) {
-              subTitle = val;
-            },
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            CustomContainer(
+              icon: Icons.check,
+              onPressed: () {
+                widget.note.title = titleController.text;
+                widget.note.subtitle = subTitleController.text;
+                widget.note.save();
+                Navigator.pop(context);
+                BlocProvider.of<GetNoteCubit>(context).getAllNote();
+              },
+            ),
+            SizedBox(height: 12),
+            CustomTextField(
+              controller:titleController ,
+              text: widget.note.title,
+            ),
+            SizedBox(height: 16),
+            CustomTextField(
+              controller: subTitleController ,
+              text: widget.note.subtitle,
+              minLine: 1,
+              maxLine:null,
+            ),
+          ],
+        ),
       ),
     );
   }
